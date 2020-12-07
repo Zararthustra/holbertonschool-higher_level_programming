97fe35b689c08cbc2525f2d1e68c68a42ca838e0
Arthur <arthmayer@outlook.fr>
2020-12-07 11:18:11 +0000

    fix checks

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-s1 = " better than "
-s2 = "If the implementation is "
-s3 = " to explain, it"
-s4 = " idea"
-s5 = "Although "
-s6 = "obvious "
-print("The Zen of Python, by Tim Peters\n\nBeautiful is{}ugly.\nExplicit is{}implicit.\nSimple is{}complex.\nComplex is{}complicated.\nFlat is{}nested.\nSparse is{}dense.\nReadability counts.\nSpecial cases aren't special enough to break the rules.\nAlthough practicality beats purity.\nErrors should never pass silently.\nUnless explicitly silenced.\nIn the face of ambiguity, refuse the temptation to guess.\nThere should be one-- and preferably only one --{}way to do it.\n{}that way may not be {}at first unless you're Dutch.\nNow is{}never.\n{}never is often{}*right* now.\n{}hard{}'s a bad{}.\n{}easy{} may be a good{}.\nNamespaces are one honking great{} -- let's do more of those!".format(s1,s1,s1,s1,s1,s1,s6,s5,s6,s1,s5,s1,s2,s3,s4,s2,s3,s4,s4))
+import this
