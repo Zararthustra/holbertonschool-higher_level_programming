888fcdbb1e18da84dae14c0bb16b20a30bf71575
Arthur <arthmayer@outlook.fr>
2020-12-08 11:43:31 +0000

    fix pep8

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for a in reversed(range(97, 123)):
-    print('{}'.format(chr(a) if a % 2 == 0 else chr(a - 32)), end = "")
+    print('{}'.format(chr(a) if a % 2 == 0 else chr(a - 32)), end="")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for a in range (0, 99):
-    print('{:d}{:d}, '.format(int(a / 10), int(a % 10)), end = "")
+for a in range(0, 99):
+    print('{:d}{:d}, '.format(int(a / 10), int(a % 10)), end="")
 print('99')
