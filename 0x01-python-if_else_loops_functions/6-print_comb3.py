6ad5fc7d414e61b5ec7b555871b7f3178f7a0264
Arthur <arthmayer@outlook.fr>
2020-12-08 12:38:05 +0000

    fix q6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for a in range(0, 10):
-    print('{:d}{:d}, '.format(int(a / 10), int(a % 10)), end="")
+    print('{:d}{:d}'.format(int(a / 10), int(a % 10)), end=", ")
 for a in range(12, 80):
     if a / 10 < a % 10:
-        print('{:d}{:d}, '.format(int(a / 10), int(a % 10)), end="")
-print('89')
+        print('{:d}{:d}'.format(int(a / 10), int(a % 10)), end=", ")
+print("89")
