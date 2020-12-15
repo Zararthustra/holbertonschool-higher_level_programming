112a309ac24510145bf3b5a57e33616614940d3f
Arthur <arthmayer@outlook.fr>
2020-12-15 15:23:38 +0000

    add q6 q7

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        print("{}".format(matrix[i]))
+    for line in matrix:
+        for i in range(len(line)):
+            if i == len(line) - 1:
+                print("{:d}".format(line[i]))
+            else:
+                print("{:d}".format(line[i]), end=" ")
