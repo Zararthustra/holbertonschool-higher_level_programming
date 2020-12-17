9491d8ceeb00b306796c7608e1fe184d5902919a
Arthur <arthmayer@outlook.fr>
2020-12-17 12:51:29 +0000

    add q1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     my_list2 = my_list.copy()
-    for  item in my_list2:
+    for item in my_list2:
         if item == search:
             my_list2[my_list2.index(item)] = replace
     return (my_list2)
