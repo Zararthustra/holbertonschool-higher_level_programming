345b0206ac1220a4d075fde70c1d4da334c376b9
Arthur <arthmayer@outlook.fr>
2020-12-17 17:42:07 +0000

    fix q10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None or a_dictionary == {}:
+    if a_dictionary is None or a_dictionary == {}:
+        return (None)
     maxi = max(a_dictionary, key=lambda k: a_dictionary[k])
     return (a_dictionary[maxi])
