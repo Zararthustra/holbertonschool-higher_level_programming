9d13f80b358ebd0833a0f58076e1a33c8f790e16
Arthur <arthmayer@outlook.fr>
2020-12-17 20:24:05 +0000

    fix q12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,12 @@
 def roman_to_int(roman_string):
     dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     add = 0
-    if roman_string is None or isinstance(roman_string, bytes):
+    if roman_string is None or not isinstance(roman_string, str):
         return (0)
     if len(roman_string) > 1:
         for i in range(len(roman_string)):
             if i > 0 and dic[roman_string[i - 1]] < dic[roman_string[i]]:
-                add -= 2 * dic[roman_string[i - 1]] 
+                add -= 2 * dic[roman_string[i - 1]]
             add += dic[roman_string[i]]
         return (add)
     return (dic[roman_string])
