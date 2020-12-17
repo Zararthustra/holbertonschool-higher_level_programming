c7b88b31fbad5f32aec280e59fe5c5d023c3ff85
Arthur <arthmayer@outlook.fr>
2020-12-17 17:52:32 +0000

    fix q11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     new = my_list.copy()
-    new = [x * number for x in new]
+    new = list(map((number).__mul__, new))
     return (new)
