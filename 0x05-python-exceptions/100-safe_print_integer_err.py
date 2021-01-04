8488df4ed61eecd8fc00907ac1d083b1c3d10417
Arthur <arthmayer@outlook.fr>
2021-01-04 16:42:44 +0000

    fix q100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
+        return (True)
     except Exception as stderr:
-        sys.stderr.write("Exception: {}".format(stderr))
+        sys.stderr.write("Exception: {}\n".format(stderr))
         return (False)
-    return (True)
