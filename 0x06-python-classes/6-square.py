2068352a8a4d20b743e03f72cfe7598d951f209e
Arthur <arthmayer@outlook.fr>
2021-01-05 20:07:50 +0000

    q0-q6 without doc

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if type(value) is tuple and len(value) == 2:
+            if type(value[0]) is int and type(value[1]) is int:
+                if value[0] >= 0 and value[1] >= 0:
+                    self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def my_print(self):
+        size = self.__size
+        position = self.__position
+        if size == 0:
+            print("")
+        else:
+            for n in range(position[1]):
+                print("")
+            for i in range(size):
+                for j in range(position[0]):
+                    print(' ', end="")
+                for k in range(size):
+                    print('#', end="")
+                print("")
