cdfb5efa3a6422509ed586078a82fedaf28e27d8
Arthur <arthmayer@outlook.fr>
2021-01-27 18:40:08 +0000

    fix q4 and q7 tests

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
 function that returns True if the object is an instance
-of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False
+of a class that inherited (directly or indirectly) from
+the specified class ; otherwise False
 """
 
 
