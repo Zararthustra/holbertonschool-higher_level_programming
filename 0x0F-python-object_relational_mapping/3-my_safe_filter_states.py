50b8e0d6cdb2438e4d30d99c6eec76afc9267d6a
Arthur <arthmayer@outlook.fr>
2021-03-25 19:32:46 +0000

    add q0 to q6

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument safe from sql injection
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_search = sys.argv[4]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (state_search, ))
+    query_rows = cur.fetchall()
+    
+    for row in query_rows:
+        print(row)
+    
+    cur.close()
+    conn.close()
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+ORM
+class definition of a State and an instance Base = declarative_base()
+"""
+
+import sys
+import MySQLdb
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    state class inherits from declarative base
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name =  Column(String(128), nullable=False)
