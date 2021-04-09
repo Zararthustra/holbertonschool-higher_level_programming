3f3e7d60647aef2c2526fae3c8ebabb6018b80f3
Arthur <arthmayer@outlook.fr>
2021-04-09 15:25:16 +0000

    add q10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+python script takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+
+    url = 'https://api.github.com/user' 
+    r = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+    jstring = r.json()
+    print(jstring.get('id'))
