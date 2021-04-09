61c82809cae344cad5ad05a21ef247a897887af2
Arthur <arthmayer@outlook.fr>
2021-04-09 15:13:36 +0000

    add q7 q8

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+python script If the HTTP status code is greater than
+or equal to 400, print: Error code: followed by the
+value of the HTTP status code
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+
+    r = requests.get(argv[1])
+    status = r.status_code
+    if status >= 400:
+        print("Error code: {}".format(status))
+    else:
+        print(r.text)
---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+python script takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user
+with the letter as a parameter
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+
+    data = {}
+    if len(argv) >= 2:
+        data['q'] = argv[1]
+    else:
+        data['q'] = ""
+        r = requests.post('http://0.0.0.0:5000/search_user', data=data)
+        try:
+            jstring = r.json()
+            
+            if jstring == {}:
+                print("No result")
+            else:
+                print("[{}] {}".format(jstring['id'], jstring['name']))
+        except Exception as err:
+            print("Not a valid JSON")
